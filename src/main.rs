44efbb118581bb63accfc6b14796605f2a1211c0
Penguin-SAMA <496107146@qq.com>
2025-08-22 18:30:45 +0800

    project init - creat an image.ppm

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let image_width_f64 = image_width as f64;
+            let image_height_f64 = image_height as f64;
+
+            let r = i as f64 / image_width_f64;
+            let g = j as f64 / image_height_f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
+}
