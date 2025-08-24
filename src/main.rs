27ce8a466ecf5b4215423e4c21932ff48c9df04c
Penguin-SAMA <496107146@qq.com>
2025-08-24 12:06:00 +0800

    add ray.rs && draw a blue-to-white image

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
+#![allow(unused)]
 use std::io::BufWriter;
 
 use indicatif::{ProgressBar, ProgressStyle};
 
 mod color;
 use color::Color;
+mod ray;
+use ray::Ray;
 mod vec3;
+use vec3::{Point3, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = vec3::unit(r.direction());
+    let a = (unit_direction.y + 1.0) * 0.5;
+    Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
+}
 
 fn main() {
     // Image
 
-    let image_width: usize = 256;
-    let image_height: usize = 256;
-
-    // let inv_w = 1.0 / (image_width - 1) as f64;
-    // let inv_h = 1.0 / (image_height - 1) as f64;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let mut image_height: i32 = (image_width as f64 / aspect_ratio) as i32;
+    image_height = if image_height < 1 { 1 } else { image_height };
 
     // indicatif
     let pb = ProgressBar::new(image_height as u64);
@@ -24,6 +33,22 @@ fn main() {
         .unwrap()
         .progress_chars("#>-"),
     );
+
+    // Camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::default();
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2. - viewport_v / 2.;
+    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
     // stdout
     let stdout = std::io::stdout();
@@ -37,11 +62,11 @@ fn main() {
 
     for j in 0..image_height {
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.,
-            );
+            let pixel_center = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+
+            let pixel_color = ray_color(&r);
             color::write_color(&mut out, pixel_color).unwrap();
         }
         pb.inc(1);
